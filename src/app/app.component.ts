99a78d528bddb542d16753292cecfd225f2940a9
Juan Guillermo Rozo <juanrozo@MacBook-Pro.local>
2020-05-29 09:04:15 -0500

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'twitter-frontend';
+export class AppComponent implements OnInit {
+  tweets: any[] = [];
+  title = 'twitter';
+  loading = false;
+  empty = false;
+
+  ngOnInit(): void {
+  }
+
+  createTweets(tweets){
+    this.tweets = tweets;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { CardComponent } from './component/card/card.component';
+import { SearchComponent } from './component/search/search.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { TwitterApiService } from './services/twitter-api.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    SearchComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [TwitterApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
